29c54af83f536ec7721c6ebef4eb873bbfc54531
Jeremy Coulon <jeremy.coulon@free.fr>
2013-01-12 15:24:48 +0100

    CMake: mark some variables as advanced

--- DefaultBuildFlags.cmake
+++ DefaultBuildFlags.cmake
@@ -10,4 +10,5 @@ if((NOT DEFINED CMAKE_BUILD_TYPE) OR (CMAKE_BUILD_TYPE STREQUAL ""))
   # CMake defaults to leaving CMAKE_BUILD_TYPE empty. This screws up
   # differentiation between debug and release builds.
   set(CMAKE_BUILD_TYPE "RelWithDebInfo" CACHE STRING "Choose the type of build, options are: None (CMAKE_CXX_FLAGS or CMAKE_C_FLAGS used) Debug Release RelWithDebInfo MinSizeRel." FORCE)
+  mark_as_advanced(CMAKE_BUILD_TYPE)
 endif()
